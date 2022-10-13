e37e989bf39d65e817f519c6a2f97b461695e8c1
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 18:07:35 +0500

    Create the smartphone`s app database models

--- smartphone_app/models.py
+++ smartphone_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    RAM = models.IntegerField()
+    memory = models.IntegerField()
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    img_url = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
