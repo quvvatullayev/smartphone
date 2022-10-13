64131b8537b7df942b27a03b88a0e9ab02a1fa73
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 19:26:57 +0500

    allowed hosts

--- smartphone/settings.py
+++ smartphone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ihkcshp6%)zskw+k-d6e^1g(b96wfy1stjdwax4@8@$flasvt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,7 +44,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
