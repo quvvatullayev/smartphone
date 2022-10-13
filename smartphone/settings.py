0e7e0fb0d5bc5f73d4831a07f68ad1bda972986c
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 18:32:56 +0500

    fix install_app

--- smartphone/settings.py
+++ smartphone/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'smartphone_app.apps.SmartphoneAppConfig',
 ]
 
 MIDDLEWARE = [
-    'smartphone_app.apps.SmartphoneAppConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
