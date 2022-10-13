33905e04118813943ae5f4f136b0d99a4a3b48e9
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 18:33:27 +0500

    add url route

--- smartphone/urls.py
+++ smartphone/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from smartphone_app.views import get_products
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', get_products),
 ]
