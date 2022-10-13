b431b1652dcd6ab178f22c57d33512a2a91f05ae
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 19:23:57 +0500

    fix models

--- smartphone_app/views.py
+++ smartphone_app/views.py
@@ -60,3 +60,40 @@ def get_product(request, id):
 
     return JsonResponse({'product': product_json})
 
+
+def add_product(request):
+    """
+    Create a product
+    args:
+        request: the request object
+    return:
+        JsonResponse: the product
+    """
+    if request.method == 'POST':
+        product = Product.objects.create(
+            name=request.POST['name'],
+            company=request.POST['company'],
+            color=request.POST['color'],
+            RAM=request.POST['RAM'],
+            memory=request.POST['memory'],
+            price=request.POST['price'],
+            img_url=request.POST['img_url'],
+        )
+        product_json = {
+            'id': product.id,
+            'name': product.name,
+            'company': product.company,
+            'color': product.color,
+            'RAM': product.RAM,
+            'memory': product.memory,
+            'price': product.price,
+            'created_at': product.created_at,
+            'updated_at': product.updated_at,
+            'img_url': product.img_url,
+        }
+        product.save() # save the product to the database
+        
+        return JsonResponse({'product': product_json})
+
+    return JsonResponse({'product': {}})
+
