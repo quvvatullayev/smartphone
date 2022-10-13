8c9779df33b33805d5edcc3ad420834a29dc2c1f
ZARIFMAC <uzbzarif@gmail.com>
2022-10-13 18:48:56 +0500

    get all products

--- smartphone_app/views.py
+++ smartphone_app/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-
+from .models import Product
 def get_products(request):
     """
     Get all products
@@ -8,7 +8,23 @@ def get_products(request):
     return:
         JsonResponse: the list of products
     """
-    
+    products = Product.objects.all()
+    products_json = []
+    for product in products:
+        products_json.append({
+            'id': product.id,
+            'name': product.name,
+            'company': product.company,
+            'color': product.color,
+            'RAM': product.RAM,
+            'memory': product.memory,
+            'price': product.price,
+            'created_at': product.created_at,
+            'updated_at': product.updated_at,
+            'img_url': product.img_url,
+            
+        })
 
-    return JsonResponse({'products': []})
+
+    return JsonResponse({'products': products_json})
 
