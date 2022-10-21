36333c90f88552837941ba9d07f2e056f9aa33da
tajriba <quvvatullayev@gmail.com>
2022-10-21 10:56:12 +0300

    templates

--- smartphone/settings.py
+++ smartphone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'smartphone_app.apps.SmartphoneAppConfig',
+    'smartphone_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'smartphone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
